6e570d16f5017f195dfb35f952c1b16510aa814a
Kenneth Choe <kenneth.choe@gmail.com>
2020-05-05 22:53:39 -0500

    Update Parameterized Sqls.sql

--- Parameterized Sqls.sql
+++ Parameterized Sqls.sql
@@ -1,2 +1,8 @@
 select ServerIdFromHandlebars = '{{ServerId}}', -- use this style if it cannot be parameterized
-       ServerIdFromParameters = @ServerId       -- recommended way. handles escaping characters automatically
+       ServerIdFromParameters = @ServerId,       -- recommended way. handles escaping characters automatically
+       ServerName = @ServerName,
+       Description = @Description,
+       UserData1 = @UserData1,
+       UserData2 = @UserData2
+       
+       -- you can expose public server info only. you cannot expose DataSource nor any credentials.
