e9bd7bb2b11b956c4dbfb13e9a2469f045aafd0f
Kenneth Choe <kenneth.choe@gmail.com>
2020-05-05 22:48:45 -0500

    Update Parameterized Sqls.sql

--- Parameterized Sqls.sql
+++ Parameterized Sqls.sql
@@ -1,3 +1,2 @@
-select ServerIdFromHandlebars = '{{ServerId}}', -- don't do this if value can contain '
-       ServerIdFromParameters = @ServerId,
-       DataSource = @DataSource
+select ServerIdFromHandlebars = '{{ServerId}}', -- use this style if it cannot be parameterized
+       ServerIdFromParameters = @ServerId       -- recommended way. handles escaping characters automatically
